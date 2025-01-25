5d2fc083c5028bf7995239ad99507d4a3bd79452
Gabriel Zuntini <gabriel99-zun@hotmail.com>
2025-01-25 18:05:57 -0300

    implementando função adicionarAmigos

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 let amigos = [];
 
+function adicionarAmigo(amigos){
+    let inserirAmigos = document.querySelector('input').value;
+    inserirAmigos.innerHTML = '';
+    
+    if(inserirAmigos == ''){
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(inserirAmigos);
+        limparCampo();
+    }
+}
+
+function limparCampo(){
+    let limparCampo = document.querySelector('input');
+    limparCampo.value = '';
+}

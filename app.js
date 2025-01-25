44443659b91bab58e6618db6cae8bd05627dbc88
Gabriel Zuntini <gabriel99-zun@hotmail.com>
2025-01-25 18:43:08 -0300

    realizado alterações na função adicionar amigos e criei função verificar amigos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let amigos = [];
 
-function adicionarAmigo(amigos){
+function adicionarAmigo(){
     let inserirAmigos = document.querySelector('input').value;
     inserirAmigos.innerHTML = '';
     
@@ -9,10 +9,23 @@ function adicionarAmigo(amigos){
     } else {
         amigos.push(inserirAmigos);
         limparCampo();
+        verificarAmigos();
     }
 }
 
 function limparCampo(){
     let limparCampo = document.querySelector('input');
     limparCampo.value = '';
+}
+
+function verificarAmigos() {
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
+
+    for(let i = 0; i < amigos.length; i++){
+        let elementosDaLista = document.createElement('li');
+        elementosDaLista.textContent = amigos[i];
+        lista.appendChild(elementosDaLista);
+        
+    }
 }
